c9ab55b400a1c066c0eac42fd6b1a0da38b4859a
sarangn19 <sarangn19@gmail.com>
2026-06-23 23:32:38 +0530

    style: add shadow to news card

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -367,6 +367,11 @@ const styles = StyleSheet.create({
     padding: 12,
     flexDirection: 'row',
     gap: 19,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.08,
+    shadowRadius: 4,
+    elevation: 2,
   },
   caImagePlaceholder: {
     width: 119,
